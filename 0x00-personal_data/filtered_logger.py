6236bda6016d217983be06d65828a6e5e38c248d
Dominic Muuo <124082859+dominic6809@users.noreply.github.com>
2024-11-06 15:32:37 +0300

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,9 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
     """
     Returns the log message obfuscated
     Args:
@@ -19,5 +21,9 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         The obfuscated log message
     """
     for field in fields:
-        message = re.sub(f'{field}=.*?{separator}', f'{field}={redaction}{separator}', message)
+        message = re.sub(
+    f'{field}=.*?{separator}',
+    f'{field}={redaction}{separator}',
+    message,
+)
     return message
