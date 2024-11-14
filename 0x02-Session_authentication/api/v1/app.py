f58e292ee87d8ac16d5aa218d0cd063fcdc990e5
Dominic Muuo <124082859+dominic6809@users.noreply.github.com>
2024-11-14 10:37:17 +0300

    Main application module for API.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
 Main application module for API.
-Sets up the Flask app and configures basic authentication for user endpoints.
+Sets up the Flask app and configures
+basic authentication for user endpoints.
 """
 import os
 from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
@@ -18,8 +20,8 @@ from api.v1.auth.session_exp_auth import SessionExpAuth
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth_type = getenv('AUTH_TYPE', 'auth')
 auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
     auth = Auth()
 if auth_type == 'basic_auth':
@@ -43,7 +45,7 @@ def not_found(error) -> str:
 @app.errorhandler(401)
 def unauthorized(error) -> str:
     """
-    Unauthorized errorhandler.
+    Unauthorized handler.
     """
     return jsonify({"error": "Unauthorized"}), 401
 
@@ -51,7 +53,7 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """
-    Forbidden errorhandler.
+    Forbidden handler.
     """
     return jsonify({"error": "Forbidden"}), 403
 
@@ -76,6 +78,12 @@ def authenticate_user():
             if user is None:
                 abort(403)
             request.current_user = user
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
 
 
 if __name__ == "__main__":
